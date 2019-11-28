1f095c6038c6d04dc73ad1096b3f7a1ff1b7490a
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-11-28 14:31:41 +0700

    up

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
     siteTitle: "NHA TRANG điểm hẹn", // Site title.
     siteTitleAlt: "Chuyện mới kể..", // Alternative site title for SEO.
     siteLogo: "https://api.adorable.io/avatars/150/logo.png", // Logo used for SEO and manifest.
-    siteUrl: "https://tupleutech.com/", // Domain of your website without pathPrefix.
+    siteUrl: "https://vtechlink.com/", // Domain of your website without pathPrefix.
     pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
     siteDescription: "Nhóm của Bình.", // Website description used for RSS feeds/meta description tag.
     siteRss: "/rss.xml", // Path to the RSS file.
